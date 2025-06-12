1e8220930ed5997b5581c108fb11a7279d667f1e
Jude Chuks <judechuks26@gmail.com>
2025-06-12 20:23:37 +0100

    sendToken helper function

---
+++ backend/utils/jwtToken.js
@@ -0,0 +1,24 @@
+export const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
+  // option for cookies
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.EXPIRE_COOKIE * 24 * 60 * 60 * 1000
+    ), // cookie expiration time
+    httpOnly: true, // cookie is not accessible via JavaScript only by the server
+  };
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({
+      success: true,
+      message: "User authenticated successfully",
+      user: {
+        name: user.name,
+        phone: user.phone,
+        email: user.email,
+        avatar: user.avatar,
+        role: user.role,
+      },
+    });
+};

4ffb23897815f0e43b5fc5a0939a4d88d334168f
Jude Chuks <judechuks26@gmail.com>
2025-06-12 12:58:05 +0100

    removing the catch block as  it has already been handled in the server.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,12 @@
 import mongoose from "mongoose";
 
 const connectMongoDatabase = () => {
-  mongoose
-    .connect(process.env.MONGODB_URI)
-    .then((data) => {
-      console.log(`MongoDB connected with server: ${data.connection.host}`);
-    })
-    .catch((error) => {
-      console.error(`Error connecting to MongoDB: ${error.message}`);
-    });
+  mongoose.connect(process.env.MONGODB_URI).then((data) => {
+    console.log(`MongoDB connected with server: ${data.connection.host}`);
+  });
+  // .catch((error) => {
+  //   console.error(`Error connecting to MongoDB: ${error.message}`);
+  // });
+  // already handled in server.js - unhandledRejection
 };
 export default connectMongoDatabase;

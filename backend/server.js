96256bf327b115cad7469d2c8b25470d2732d29f
Jude Chuks <judechuks26@gmail.com>
2025-06-12 13:18:05 +0100

    handling uncaught exception error

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,14 @@ import connectMongoDatabase from "./config/db.js";
 
 dotenv.config({ path: "backend/config/config.env" }); // setting the path for the env file
 connectMongoDatabase(); // connect to MongoDB database
+
+// handle uncaught exception error - like an undefined variable in the server.js file
+process.on("uncaughtException", (err) => {
+  console.error(`Error: ${err.message}`);
+  console.warn(`Server is shutting down due to uncaught exception errors`);
+  process.exit(1);
+});
+
 const port = process.env.PORT || 3000;
 
 const server = app.listen(process.env.PORT, () => {
@@ -18,3 +26,5 @@ process.on("unhandledRejection", (err) => {
     process.exit(1); // Exit the process with failure
   });
 });
+
+p;

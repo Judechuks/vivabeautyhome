c983e51cce5a055306a40daaa2c8d2b2be261441
Jude Chuks <judechuks26@gmail.com>
2025-06-16 18:12:08 +0100

    actions on products

--- backend/controller/productController.js
+++ backend/controller/productController.js
@@ -5,6 +5,7 @@ import APIFunctionality from "../utils/apiFunctionality.js";
 
 // 1. creating products
 export const createProducts = async (req, res) => {
+  req.body.user = req.user.id;
   const product = await productModel.create(req.body);
   res
     .status(201)
@@ -97,4 +98,103 @@ export const getSingleProduct = handleAsyncError(async (req, res, next) => {
       product,
     });
   }
+});
+
+// 6. Admin - Getting all the products
+export const getAdminProducts = handleAsyncError(async (req, res, next) => {
+  const products = await productModel.find();
+  res.status(200).json({
+    success: true,
+    message: "Products fetched successfully",
+    products,
+  });
+});
+
+// 7. Creating and updating review
+export const createReviewForProduct = handleAsyncError(
+  async (req, res, next) => {
+    const { rating, comment, productId } = req.body;
+    const review = {
+      user: req.user._id,
+      name: req.user.name,
+      rating: Number(rating),
+      comment,
+    };
+    const product = await productModel.findById(productId);
+    if (!product) {
+      next(new HandleError("Product does not exist", 400));
+    }
+    const reviewExists = product.reviews.find(
+      (review) => review.user.toString() === req.user.id.toString() // user that is logged in
+    );
+    console.log("reviewExists", reviewExists);
+
+    if (reviewExists) {
+      // edit the review if it already exists
+      product.reviews.forEach((review) => {
+        if (review.user.toString() === req.user.id.toString()) {
+          review.rating = rating;
+          review.comment = comment;
+        }
+      });
+    } else {
+      product.reviews.push(review); // add the review if it doesn't exist
+    }
+    product.numberOfReviews = product.reviews.length;
+    let sum = 0;
+    product.reviews.forEach((review) => {
+      sum += review.rating;
+    });
+    product.rating =
+      product.reviews.length > 0 ? sum / product.reviews.length : 0;
+    await product.save({ validateBeforeSave: false });
+    res.status(200).json({
+      success: true,
+      message: "Review added successfully",
+      product,
+    });
+  }
+);
+
+// 8. Getting product reviews
+export const getProductReviews = handleAsyncError(async (req, res, next) => {
+  const product = await productModel.findById(req.query.id);
+  if (!product) {
+    return next(new HandleError("Product not found", 404));
+  }
+  res.status(200).json({
+    success: true,
+    message: "Reviews fetched successfully",
+    reviews: product.reviews,
+  });
+});
+
+// 9. Deleting product review
+export const deleteReview = handleAsyncError(async (req, res, next) => {
+  const product = await productModel.findById(req.query.productId);
+  if (!product) {
+    return next(new HandleError("Product not found", 404));
+  }
+  const reviews = product.reviews.filter(
+    (review) => review._id.toString() !== req.query.id.toString()
+  );
+  let sum = 0;
+  reviews.forEach((review) => {
+    sum += review.rating;
+  });
+  const ratings = reviews.length > 0 ? sum / reviews.length : 0;
+  const numberOfReviews = reviews.length;
+  await productModel.findByIdAndUpdate(
+    req.query.productId,
+    {
+      reviews,
+      ratings,
+      numberOfReviews,
+    },
+    { new: true, runValidators: true }
+  );
+  res.status(200).json({
+    success: true,
+    message: "Review deleted successfully",
+  });
 });

17d54ad3a7a9119dd8710e68ca103a0b3cf84743
Jude Chuks <judechuks26@gmail.com>
2025-06-16 18:00:18 +0100

    registering user and order route

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 // const express = require("express");
 import express from "express"; // to use this, add "type":"module" in the package.json
+import cookieParser from "cookie-parser";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
 import HandleErrorMiddleware from "./middleware/error.js";
 const app = express(); // for defining routes and middleware
 
@@ -13,9 +16,12 @@ const app = express(); // for defining routes and middleware
 
 // Middleware to parse JSON bodies
 app.use(express.json()); // to parse JSON data in the request body
+app.use(cookieParser()); // to have access to cookies from the req.cookies
 
 // Route using middlewares
 app.use("/api/v1", productRoutes);
+app.use("/api/v1", userRoutes);
+app.use("/api/v1", orderRoutes);
 
 // error middleware
 app.use(HandleErrorMiddleware);

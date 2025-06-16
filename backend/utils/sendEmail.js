b6b68bb39867faeb25f0d552ceb64e325edbd05a
Jude Chuks <judechuks26@gmail.com>
2025-06-16 18:06:50 +0100

    send email helper function

---
+++ backend/utils/sendEmail.js
@@ -0,0 +1,21 @@
+import nodemailer from "nodemailer";
+
+export const sendEmail = async (options) => {
+  const transporter = nodemailer.createTransport({
+    service: process.env.SMTP_SERVICE,
+    auth: {
+      user: process.env.SMTP_MAIL,
+      pass: process.env.SMTP_PASSWORD,
+    },
+    tls: {
+      rejectUnauthorized: false, // Ignore self-signed certificate errors
+    },
+  });
+  const mailOptions = {
+    from: process.env.SMTP_MAIL,
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+  };
+  await transporter.sendMail(mailOptions);
+};

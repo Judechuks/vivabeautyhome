73f7662e847ad65d77ee8b2128cf7f0e223f08bf
Judechuks <judechuks@gmail.com>
2026-04-13 09:23:56 +0100

    inline padding for the page layout body

--- frontend/src/pages/layout/PublicPageLayout.jsx
+++ frontend/src/pages/layout/PublicPageLayout.jsx
@@ -6,7 +6,7 @@ const PublicPageLayout = () => {
   return (
     <div className="page-layout grid min-h-dvh bg-gray-50 dark:bg-gray-900 dark:text-white">
       <Header />
-      <main className="">
+      <main className="px-3 md:px-5">
         <Outlet />
       </main>
       <Footer />
